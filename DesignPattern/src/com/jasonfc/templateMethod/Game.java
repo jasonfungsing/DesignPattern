281a66703d830cb206967675e3728a942bfe720d
Jason Fung <jasonfungsing@gmail.com>
2012-02-22 21:32:54 +1100

    Update sysout with additional info for template method pattern

--- DesignPattern/src/com/jasonfc/templateMethod/Game.java
+++ DesignPattern/src/com/jasonfc/templateMethod/Game.java
@@ -9,10 +9,16 @@ public abstract class Game {
 	abstract void printWinner();
 	
 	public final void TemplateMethod(){
+		System.out.print("How to start games: ");
 		initializeGame();
+		System.out.print("First step: ");
 		keyStep1();
+		System.out.print("Second step: ");
 		keyStep2();
+		System.out.println("Omit steps....");
+		System.out.print("Decide who is winner: ");
 		decideWin();
+		System.out.print("The winner is: ");
 		printWinner();
 	}
 }
